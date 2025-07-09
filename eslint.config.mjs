ab184aff5d8a2109395232dec61e24543efd3ec1
excess-ca <150656390+excess-ca@users.noreply.github.com>
2025-07-09 11:04:57 +0800

    * yarn lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,56 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import globals from "globals";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import globals from 'globals'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 export default [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
-	{
-		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-		plugins: {
-			js,
-		},
-		languageOptions: {
-			globals: globals.browser,
-		},
-		rules: {
-			// * no-console rule
-			"no-console": [
-				"warn",
-				{
-					allow: ["warn", "error"],
-				},
-			],
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {
+      js
+    },
+    languageOptions: {
+      globals: globals.browser
+    },
+    rules: {
+      // * no-console rule
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error']
+        }
+      ],
 
-			// * no-unused-vars rule
-			"no-unused-vars": [
-				"warn",
-				{
-					vars: "all",
-					args: "after-used",
-					caughtErrors: "all",
-					ignoreRestSiblings: true,
-					reportUsedIgnorePattern: false,
-				},
-			],
+      // * no-unused-vars rule
+      'no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          caughtErrors: 'all',
+          ignoreRestSiblings: true,
+          reportUsedIgnorePattern: false
+        }
+      ],
 
-			// * require-default-props rule
-			"react/require-default-props": [
-				"warn",
-				{
-					forbidDefaultForRequired: true,
-					ignoreFunctionalComponents: true,
-				},
-			],
-		},
-	},
-];
+      // * require-default-props rule
+      'react/require-default-props': [
+        'warn',
+        {
+          forbidDefaultForRequired: true,
+          ignoreFunctionalComponents: true
+        }
+      ]
+    }
+  }
+]
